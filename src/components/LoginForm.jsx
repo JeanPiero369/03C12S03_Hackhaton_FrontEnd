9d87429d5c3394c253f5c77ec9a510191dae37cc
unknown <jean.huaylla@utec.edu.pe>
2024-06-22 15:17:40 -0500

    first

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,15 @@
 import {InputForm} from "./InputForm.jsx";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {fetchLogin} from "../service/api.js";
 
 export const LoginForm=()=>{
     const [data,setData]=useState({
         username : "aa",
         password:""
     });
+
+    const navigate = useNavigate();
 
     const handleChangeInput=(data2)=>{
         const { name, value } = data2;
@@ -15,10 +19,27 @@ export const LoginForm=()=>{
             [name]: value
         });
     }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await fetchLogin(data);
+            navigate('/dashboard');
+        } catch (error) {
+            navigate('/auth/login');
+        }
+    };
 
 
     return<>
-        <InputForm initial={data.username} label="Username" name="username" onDataChange={handleChangeInput}/>
-        <InputForm initial={data.password}  label="Password" name="password" onDataChange={handleChangeInput}/>
+        <form onSubmit={handleSubmit}>
+            <InputForm initial={data.username} label="Username" name="username" onDataChange={handleChangeInput}/>
+            <InputForm initial={data.password} label="Password" name="password" onDataChange={handleChangeInput}/>
+            <button
+                id="loginSubmit"
+                className="bg-primary text-white font-bold py-2 px-4 rounded-full w-full mb-4"
+                type="submit">
+                Iniciar Sesión
+            </button>
+        </form>
     </>
 }
